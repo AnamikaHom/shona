81eaaec87645bc76c94dd8d7d9e1f1a61ae59612
AnamikaHom <116956971+AnamikaHom@users.noreply.github.com>
2022-10-29 14:48:58 +0200

    p1 change

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ basic.forever(function () {
     if (tinkercademy.PIR(DigitalPin.P0)) {
         tinkercademy.LED(DigitalPin.P1, OnOff.On)
     } else {
-        tinkercademy.LED(DigitalPin.P1, OnOff.Off)
+        tinkercademy.LED(DigitalPin.P1, OnOff.On)
     }
 })
